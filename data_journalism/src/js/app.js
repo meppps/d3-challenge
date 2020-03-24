953245c2997cf8f08aa0ff5a51f017f537659fcc
Mikey Epps <55078570+meppps@users.noreply.github.com>
2020-03-24 14:07:37 -0700

    First plot

--- data_journalism/src/js/app.js
+++ data_journalism/src/js/app.js
@@ -2,5 +2,46 @@
 
 d3.csv('src/data/data.csv').then((data)=>{
     console.log(data);
+
+    var smokes = data.map(d => Number(d.smokes));
+    var age = data.map(d => Number(d.age));
+
+    // console.log(age)
+
+    var svgWidth = 960;
+    var svgHeight = 500;
+
+    var svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+    var chartGroup = svg.append("g");
+
+    var yScale = d3.scaleLinear()
+    .domain([0, d3.max(age)])
+    .range([svgHeight, 0]);
+
+    var xScale = d3.scaleLinear()
+    .domain([0,d3.max(smokes)])
+    .range([0,svgWidth]);
+
     
+    var yAxis = d3.axisLeft(yScale);
+    var xAxis = d3.axisBottom(xScale);
+
+    chartGroup.append('g')
+    .call(yAxis);
+
+    chartGroup.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx',function(d){return xScale(Number(d.smokes))})
+    .attr('cy',function(d){return yScale(Number(d.age))})
+    .attr('r',5)
+    .style('fill','red')
+
+
 });
